ae1e2a667a5383d70f16394fe933781ae0f488c9
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2023-08-15 12:07:30 +0530

    login and logout  api done

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,6 @@ export const register = catchAsyncError(async (req, res, next) => {
     return next(new ErrorHandler("Please Enter All Fields", 400));
 
   let user = await User.findOne({ email });
-
   if (user) return next(new ErrorHandler("User Already Exist", 409));
 
   user = await User.create({
@@ -24,4 +23,32 @@ export const register = catchAsyncError(async (req, res, next) => {
   });
 
   sendToken(res, user, "Register Successfully", 201);
+});
+export const login = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return next(new ErrorHandler("Please Enter All Fields", 400));
+
+  let user = await User.findOne({ email }).select("+password");
+
+  if (!user) return next(new ErrorHandler("Icorrect Email Or Password", 401));
+
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch)
+    return next(new ErrorHandler("Icorrect Email Or Password", 401));
+
+  sendToken(res, user, `Welcome back.${user.name}`, 200);
+});
+
+export const logout = catchAsyncError(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", null, {
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "Logged out Successfully",
+    });
 });
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const schema = new mongoose.Schema({
   name: {
     type: String,
@@ -54,6 +55,13 @@ const schema = new mongoose.Schema({
   ResetPasswordExpire: String,
 });
 
+schema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const hashedPassword = await bcrypt.hash(this.password, 10);
+  this.password = hashedPassword;
+  next();
+});
+
 schema.methods.getJWTToken = function () {
   return jwt.sign(
     {
@@ -64,6 +72,10 @@ schema.methods.getJWTToken = function () {
       expiresIn: "15d",
     }
   );
+};
+schema.methods.comparePassword = async function (password) {
+    console.log(this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 export const User = mongoose.model("User", schema);
