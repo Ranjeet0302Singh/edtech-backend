885e0ef67e35bf90ea83b0b39ba15ec8592bf038
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2023-08-19 01:07:15 +0530

    Razorpay payment validation check and Payment model done

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,6 +2,8 @@ import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { User } from "../models/User.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { instance } from "../server.js";
+import crypto from "crypto";
+import { Payment } from "../models/Payment.js";
 export const buySubscription = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
 
@@ -21,6 +23,41 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
   await user.save();
   res.status(201).json({
     success: true,
-    subscriptionId:subscription.id,
+    subscriptionId: subscription.id,
+  });
+});
+export const paymentVerification = catchAsyncError(async (req, res, next) => {
+  const { razorpay_signature, razorpay_payment_id, razorpay_subscription_id } =
+    req.body;
+
+  const user = await User.findById(req.user._id);
+  const subscription_id = user.subscription.id;
+  const generated_signature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+    .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
+    .digest("hex");
+
+  const isAuthentic = generated_signature === razorpay_signature;
+
+  if (!isAuthentic)
+    return res.redirect(`${process.env.FRONTEND_URL}/paymentfail`);
+  await Payment.create({
+    razorpay_signature,
+    razorpay_payment_id,
+    razorpay_subscription_id,
+  });
+
+  user.subscription.status = "active";
+
+  await user.save();
+  res.redirect(
+    `${process.env.FRONTEND_URL}/paymentsuccess?refrence=${razorpay_payment_id}`
+  );
+});
+
+export const getRazorPayKey = catchAsyncError(async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    key: process.env.RAZORPAY_API_KEY,
   });
 });
