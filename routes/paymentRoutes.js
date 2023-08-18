fceb8874553840c859c2a0df29ebc2e42784e21b
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2023-08-19 00:33:49 +0530

    razorpay integration done paymentRoutes and Controller done

---
+++ routes/paymentRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { isAuthenticated } from "../middlewares/auth.js";
+import { buySubscription } from "../controllers/paymentController.js";
+
+const router = express.Router();
+
+router.route("/subscribe").get(isAuthenticated,buySubscription)
+
+export default router;
