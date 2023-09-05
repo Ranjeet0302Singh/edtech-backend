423f9e4ab24a577d02f5030a8385782574b4bc7e
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2023-09-05 08:31:08 +0530

    commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,29 @@
 import jwt from "jsonwebtoken";
-
 import ErrorHandler from "../utils/errorHandler.js";
-
 import { catchAsyncError } from "./catchAsyncError.js";
-
 import { User } from "../models/User.js";
 
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
   const { token } = req.cookies;
+
   if (!token) return next(new ErrorHandler("Not Logged In", 401));
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
   req.user = await User.findById(decoded._id);
+
   next();
 });
+
+export const authorizeSubscribers = (req, res, next) => {
+  if (req.user.subscription.status !== "active" && req.user.role !== "admin")
+    return next(
+      new ErrorHandler(`Only Subscribers can acces this resource`, 403)
+    );
+
+  next();
+};
+
 export const authorizeAdmin = (req, res, next) => {
   if (req.user.role !== "admin")
     return next(
@@ -23,15 +32,6 @@ export const authorizeAdmin = (req, res, next) => {
         403
       )
     );
-  next();
-};
-export const authorizeSubscribers = (req, res, next) => {
-  if (req.user.subscription.status !== "active" && req.user.role !== "admin")
-    return next(
-      new ErrorHandler(
-        `Only Subscribers can access this resoursce`,
-        403
-      )
-    );
+
   next();
 };
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,12 +1,24 @@
 import express from "express";
+import {
+  buySubscription,
+  cancelSubscription,
+  getRazorPayKey,
+  paymentVerification,
+} from "../controllers/paymentController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { buySubscription, cancelSubscription, getRazorPayKey, paymentVerification } from "../controllers/paymentController.js";
 
 const router = express.Router();
 
-router.route("/subscribe").get(isAuthenticated,buySubscription)
-router.route("/paymentverification").post(isAuthenticated,paymentVerification)
-router.route("/razorpaykey").get(getRazorPayKey)
-router.route("/subscibe/cancel").delete( isAuthenticated, cancelSubscription)
+// Buy Subscription
+router.route("/subscribe").get(isAuthenticated, buySubscription);
+
+// Verify Payment and save reference in database
+router.route("/paymentverification").post(isAuthenticated, paymentVerification);
+
+// Get Razorpay key
+router.route("/razorpaykey").get(getRazorPayKey);
+
+// Cancel Subscription
+router.route("/subscribe/cancel").delete(isAuthenticated, cancelSubscription);
 
 export default router;
